0fdb64e517c7c97f0b36fe884f630f58a70f2e1e
endangismaya <endang.ismaya@gmail.com>
2024-09-07 16:56:15 +0700

    Update UI for Image and Categories

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,7 @@
 import Calendar from "./Calendar";
 import Weather from "./Weather";
 import "./News.css";
+import userImg from "../assets/images/user.jpg";
 
 const News = () => {
   return (
@@ -18,8 +19,42 @@ const News = () => {
       </header>
       <div className="news-content">
         <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userImg} alt="User Image" />
+            <p>Mary&apos;s Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
+              <a href="#" className="nav-link">
+                General
+              </a>
+              <a href="#" className="nav-link">
+                World
+              </a>
+              <a href="#" className="nav-link">
+                Business
+              </a>
+              <a href="#" className="nav-link">
+                Technology
+              </a>
+              <a href="#" className="nav-link">
+                Entertainment
+              </a>
+              <a href="#" className="nav-link">
+                Science
+              </a>
+              <a href="#" className="nav-link">
+                Health
+              </a>
+              <a href="#" className="nav-link">
+                Nation
+              </a>
+              <a href="#" className="nav-link">
+                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         <div className="news-section">
           <div className="headline">Headline</div>
